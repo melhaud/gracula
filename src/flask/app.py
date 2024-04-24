d253d1506f85adfd4bc9239bea6655be470e1a72
melhaud <ek.e.agafonova.com>
2024-04-24 16:52:43 -0400

    requirements.txt update

--- src/flask/app.py
+++ src/flask/app.py
@@ -1,5 +1,6 @@
-from flask_frozen import Freezer
+#from flask_frozen import Freezer
 from flask import Flask, render_template, send_file
+import json
 
 DEBUG = True
 FLATPAGES_AUTO_RELOAD = DEBUG
@@ -11,8 +12,17 @@ app = Flask(__name__)
 
 @app.route("/")
 
-def home():
-    return render_template("index.html")
+def index():
+    with open("settings.txt", encoding="utf8") as base_config:
+        data = base_config.read()
+        settings = json.loads(data)
+
+    # variables = {
+    #     "title" : "Gracula Project",
+    #     "description" : "Gracula🧛‍♂️🧛‍♀️ is an AI-agent-based scientific papers extractor. It uses GigaChat API to analyse user-uploaded PDF files and build a compilation table dataset out of them.",
+    #     "keywords" : "scientific parser, gigachat"
+    # }
+    return render_template("index.html", bigheader=True, **settings)
 
 @app.route("/download")
 def download_file():
